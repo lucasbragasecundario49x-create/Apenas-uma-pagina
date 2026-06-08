707ec886432ff7a1e8e5b98bfefe3272221897c7
lucasbraga63 <lucas.b.silva63@aluno.senai.br>
2026-06-08 14:33:51 -0300

    Ok

--- script.js
+++ script.js
@@ -43,6 +43,45 @@ botaoNegado.addEventListener("mouseover", () => {
     span.textContent = "Ops! Parece que tá fugindo"
 });
 
+botaoNegado.addEventListener("click", () => {
+    if (botaoNegado.parentNode !== document.body) {
+        document.body.appendChild(botaoNegado);
+    }
+
+    const desc = descricao.getBoundingClientRect();
+
+    const larguraBotao = botaoNegado.offsetWidth;
+    const alturaBotao = botaoNegado.offsetHeight;
+
+    const larguraTela = document.documentElement.clientWidth;
+    const alturaTela = document.documentElement.clientHeight;
+
+    let x, y;
+    let valido = false;
+
+    while (!valido) {
+        x = Math.random() * (larguraTela - larguraBotao);
+        y = Math.random() * (alturaTela - alturaBotao);
+
+        const colide =
+            x < desc.right &&
+            x + larguraBotao > desc.left &&
+            y < desc.bottom &&
+            y + alturaBotao > desc.top;
+
+        if (!colide) {
+            valido = true;
+        }
+    }
+
+
+    botaoNegado.style.position = "fixed";
+    botaoNegado.style.left = `${x}px`;
+    botaoNegado.style.top = `${y}px`;
+
+    span.textContent = "Ops! Parece que tá fugindo"
+});
+
 botaoAceito.addEventListener('click', ()=> {
    img.src = "imagemAceito.jpg"
    span.textContent = "Me mande um print nessa tela agora mesmo por qualquer lugar"
